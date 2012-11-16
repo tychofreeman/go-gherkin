18d49ce6e8a247fe2055bbdba82d7d6ad12654ad
Christopher Freeman <cwfreeman@gmail.com>
2012-11-15 23:22:26 -0600

    Added comments about future needs - PyStrings, reporting, tags

--- gherkin_test.go
+++ gherkin_test.go
@@ -155,3 +155,6 @@ func TestRemembersNamesOfScenariosRun(t *testing.T) {
 
 // Need to introduce Backgrounds and Scenario Outlines/Examples and table inputs
 // Need to support a lifecycle (setup/teardown) for each scenario executed.
+// Support PyStrings?
+// Support tags?
+// Support reporting.
