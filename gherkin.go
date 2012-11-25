d35b73b76be448716f24d78f3293b9c6623d317d
Christopher Freeman <cwfreeman@gmail.com>
2012-11-25 16:58:29 -0600

    Moved stepdef to its own file.

--- gherkin.go
+++ gherkin.go
@@ -12,29 +12,6 @@ import (
     "bytes"
 )
 
-type stepdef struct {
-    r *re.Regexp
-    f func(*World)
-}
-
-func createstepdef(p string, f func(*World)) stepdef {
-    r, _ := re.Compile(p)
-    return stepdef{r, f}
-}
-
-func (s stepdef) execute(line *step, output io.Writer) bool {
-    if s.r.MatchString(line.String()) {
-        if s.f != nil {
-            substrs := s.r.FindStringSubmatch(line.String())
-            w := &World{regexParams:substrs, multiStep:line.mldata, output: output} 
-            defer func() { line.hasErrors = w.gotAnError }()
-            s.f(w)
-        }
-        return true
-    }
-    return false
-}
-
 type step struct {
     line string
     orig string
