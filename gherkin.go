1d36e16a8bfbcd2dcfc3469b21629d7fea1d596a
Christopher Freeman <cwfreeman@gmail.com>
2012-11-25 22:34:44 -0600

    Added convenience functions.

--- gherkin.go
+++ gherkin.go
@@ -27,6 +27,22 @@ func RegisterStepDef(pattern string, stepdef func(*World)) {
     DefaultRunner.RegisterStepDef(pattern, stepdef)
 }
 
+func Given(pattern string, stepdef func(*World)) {
+    DefaultRunner.RegisterStepDef(pattern, stepdef)
+}
+
+func When(pattern string, stepdef func(*World)) {
+    DefaultRunner.RegisterStepDef(pattern, stepdef)
+}
+
+func Then(pattern string, stepdef func(*World)) {
+    DefaultRunner.RegisterStepDef(pattern, stepdef)
+}
+
+func And(pattern string, stepdef func(*World)) {
+    DefaultRunner.RegisterStepDef(pattern, stepdef)
+}
+
 // Pass-through for Runner.SetOutput()
 func SetOutput(output io.Writer) {
     DefaultRunner.SetOutput(output)
