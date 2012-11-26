5739661b0ffc009a890e89e4364564ee2d39b922
Christopher Freeman <cwfreeman@gmail.com>
2012-11-25 22:15:06 -0600

    Added static definitions of Runner API to make writing tests easier.

--- gherkin.go
+++ gherkin.go
@@ -1,8 +1,39 @@
 // Support the Gherkin language, as found in Ruby's Cucumber and Python's Lettuce projects.
 package gherkin
 
+import "io"
+
+// Static Runner object to make creating tests easier
+var DefaultRunner = CreateRunner()
+
 // Use this function to let the user know that this
 // test is not complete.
 func Pending() {
     panic("Pending")
+}
+
+// Pass-through for Runner.SetSetUpFn()
+func SetUp(setup func()) {
+    DefaultRunner.SetSetUpFn(setup)
+}
+
+// Pass-through for Runner.SetTearDownFn()
+func TearDown(teardown func()) {
+    DefaultRunner.SetTearDownFn(teardown)
+}
+
+// Pass-through for Runner.RegisterStepDef()
+func RegisterStepDef(pattern string, stepdef func(*World)) {
+    DefaultRunner.RegisterStepDef(pattern, stepdef)
+}
+
+// Pass-through for Runner.SetOutput()
+func SetOutput(output io.Writer) {
+    DefaultRunner.SetOutput(output)
+}
+
+// Pass-through for Runner.Run()
+// This should be called after everything else.
+func Run() {
+    DefaultRunner.Run()
 }
